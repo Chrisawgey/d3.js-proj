69c103b4d0136b41361cb30eac3524e93a420731
Chrisawgey <124189207+Chrisawgey@users.noreply.github.com>
2023-11-24 21:46:28 -0500

    saved for u

---
+++ getData.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "your_host";
+$username = "your_user";
+$password = "your_password";
+$dbname = "your_database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM your_table";
+$result = $conn->query($sql);
+
+$data = array();
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+}
+
+$conn->close();
+
+echo json_encode($data);
+?>
